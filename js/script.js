aebd41cdccb93de5578e25c7475d601bb08b4b7a
André Seoane <AndreSeoane@users.noreply.github.com>
2021-11-08 21:23:35 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function exibirDataHora(){
+    document.getElementById('relogio').innerHTML = Date();
+}
+function exibirMensagem(){
+    document.getElementById('mensagem').innerHTML = 'Uma Super Mensagem!';
+}
+
+function alternarDataHoraPorMensagem(){
+
+}
